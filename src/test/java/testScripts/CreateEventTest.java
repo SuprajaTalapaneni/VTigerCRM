f7cb1838b9bb37483ac4af33b607dbf0d715fd00
jyoth <jyoth@LAPTOP-5H2K9VJP>
2024-03-12 13:23:22 +0530

    user1 commit

--- src/test/java/testScripts/CreateEventTest.java
+++ src/test/java/testScripts/CreateEventTest.java
@@ -13,13 +13,16 @@ public class CreateEventTest extends BaseClass {
 	public void CreateEventTest() throws InterruptedException {
 		Map<String, String> map = excel.readFromExcel("Create New Event", "EventsTestData");
 		home.selectFromQuickCreate(web, map.get("Quick Create"));
+		
 		soft.assertEquals(createEvent.getPageHeader(), "Create To Do");
+		
 		String subject = map.get("Subject") + jutil.generateRandomNum(100);
 		createEvent.setSubject(subject);
 		createEvent.setStartDate(map.get("Start Date"));
 		createEvent.setEndDate(map.get("Due Date"));
 		Thread.sleep(3000);
 		createEvent.clickSave();
+		
 		soft.assertTrue(newEvent.getPageHeader().contains(subject));
 		Thread.sleep(3000);
 		if (newEvent.getPageHeader().contains(subject)) {
